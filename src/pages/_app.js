7b8c9aa99728e0a33698cf929249dc965d93d2c3
Quan_Hoang_ <73420593+zziboyzz@users.noreply.github.com>
2021-03-11 20:17:50 +0700

    Add files via upload

---
+++ src/pages/_app.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          rel="preload"
+          as="font"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
